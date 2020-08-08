ae00fc3379b39089807f9fb662ea57d0b6e0d747
justmeben <ben.roznboim@gmail.com>
2020-08-08 22:53:13 +0300

    added js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 feather.replace()
+
+let fs_elem = document.getElementById('fs')
+fs_elem.addEventListener("click", function() {
+    fs_elem.classList.toggle('fs_hide')
+});
+
+let elementsArray = document.querySelectorAll(".gallery img");
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+        fs_elem.classList.toggle('fs_hide')
+        fs_elem.querySelector('img').src = elem.src
+    });
+});
